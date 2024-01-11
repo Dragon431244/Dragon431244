85ba8838bf8793f80dd8de9f72c8cd531244e031
Dragon431244 <143772667+Dragon431244@users.noreply.github.com>
2024-01-11 09:43:22 +0700

    Add files via upload

---
+++ Dragon X Hud.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "Dragon X Hub" -- Put You Name Script Here
+_G.Logo = "15941673729" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
